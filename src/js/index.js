95a69341053041ff155784cdc8697ee7e2674368
jamilya-dev <omg.groups@yandex.ru>
2024-09-10 14:15:14 +0300

    Выпролнено модальное окно заказ звонка

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,3 @@
 import modals from './modules/modals';
+
 modals();
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,4 +1,17 @@
 const modals = () => {
-  console.log('modals');
+  const modalBtn = document.querySelector('.button');
+  const headerModal = document.querySelector('.header-modal');
+  const overlay = document.querySelector('.overlay');
+  const modalClose = document.querySelector('.header-modal__close');
+
+  modalBtn.addEventListener('click', () => {
+    overlay.style.display = 'block';
+    headerModal.style.display = 'block';
+  });
+
+  modalClose.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    headerModal.style.display = 'none';
+  });
 };
 export default modals;
