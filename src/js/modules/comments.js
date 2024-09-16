eb0d40c8930da80e45283586647ea9a1a323ea36
jamilya-dev <omg.groups@yandex.ru>
2024-09-17 01:43:05 +0300

    дополнительное задание

--- src/js/modules/comments.js
+++ src/js/modules/comments.js
@@ -30,46 +30,76 @@ const comments = () => {
       const blockPosition = ['review-arrow-left', 'review-arrow-right'];
       let itemIndex = 0;
 
-      comments.forEach((comment) => {
-        if (itemIndex <= 2) {
-          const commentElement = document.createElement('div');
-          commentElement.classList.add('comment-item', 'review-margin-bottom', 'row');
+      const commentElements = [];
 
-          commentElement.innerHTML = `
-          <div class="col-xs-3 col-sm-2" bis_skin_checked="1">
-            <div class="review-user" bis_skin_checked="1">
+      comments.forEach((comment) => {
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('comment-item', 'review-margin-bottom', 'row');
+
+        commentElement.innerHTML = `
+          <div class="col-xs-3 col-sm-2">
+            <div class="review-user">
               <img src="images/users/${comment.image ? comment.image : 'avatar.jpg'}" alt="${comment.author}" class="img-responsive avatar">
             </div>
-          </div> 
-          <div class="col-xs-9 col-sm-9" bis_skin_checked="1">
-            <div class="review-inner review-arrow" bis_skin_checked="1">
-              <p class="text-normal">${comment.author}</p>
+          </div>
+          <div class="col-xs-9 col-sm-9">
+            <div class="review-inner review-arrow">
+              <p class="text-normal">${comment.author} ${itemIndex}</p>
               <p>${comment.comment}</p>
             </div>
           </div>`;
-          commentsContainer.append(commentElement);
 
-          const currentElement = commentElement.querySelector('.review-inner');
-          const imgItem = commentElement.querySelector('.col-xs-3.col-sm-2');
-          const bgColorIndex = itemIndex % bgColors.length;
-          const positionIndex = itemIndex % blockPosition.length;
+        commentsContainer.append(commentElement);
 
-          currentElement.classList.add(bgColors[bgColorIndex], blockPosition[positionIndex]);
+        commentElements.push(commentElement);
 
-          if (itemIndex % 2) {
-            imgItem.style.order = '1';
-            commentElement.style.display = 'flex';
-          }
-          if (itemIndex === 2) {
-            commentElement.classList.remove('review-margin-bottom');
-          }
-        }
+        const currentElement = commentElement.querySelector('.review-inner');
+        const imgItem = commentElement.querySelector('.col-xs-3.col-sm-2');
+        const bgColorIndex = itemIndex % bgColors.length;
+        const positionIndex = itemIndex % blockPosition.length;
+
+        currentElement.classList.add(bgColors[bgColorIndex], blockPosition[positionIndex]);
+
         itemIndex++;
       });
+
+      const showComments = () => {
+        commentElements.forEach((elem) => {
+          elem.style.display = 'none';
+        });
+        for (let i = 0; i < 3; i++) {
+          const indexToShow = (itemIndex + i) % commentElements.length;
+          commentElements[indexToShow].style.display = 'flex';
+
+          const imageElement = commentElements[indexToShow].querySelector('.col-xs-3.col-sm-2');
+
+          const style = document.createElement('style');
+
+          if (i === 1) {
+            imageElement.style.order = '1';
+
+            style.textContent = `
+            .review-arrow-right::after {
+                right: -39px; 
+            }`;
+            document.head.appendChild(style);
+          } else {
+            imageElement.style.order = '0';
+            style.textContent = `
+            .review-arrow-right::after {
+                right: 39px; 
+            }`;
+          }
+        }
+        itemIndex = (itemIndex + 1) % commentElements.length;
+      };
+      showComments();
       spinner.style.display = 'none';
+      setInterval(showComments, 8000);
     });
   };
 
   setData();
 };
+
 export default comments;
