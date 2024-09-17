03deaff5512626bd32d4d5a8b26cc596dfda4e0a
jamilya-dev <omg.groups@yandex.ru>
2024-09-18 00:05:38 +0300

    комментарии доработка

--- src/js/modules/comments.js
+++ src/js/modules/comments.js
@@ -26,7 +26,7 @@ const comments = () => {
 
   const setData = () => {
     getData().then((comments) => {
-      const bgColors = ['review-green', 'review-gray', 'review-orange'];
+      const bgColors = ['review-green', 'review-gray', 'review-orange', 'review-gray'];
       const blockPosition = ['review-arrow-left', 'review-arrow-right'];
       let itemIndex = 0;
 
@@ -35,6 +35,7 @@ const comments = () => {
       comments.forEach((comment) => {
         const commentElement = document.createElement('div');
         commentElement.classList.add('comment-item', 'review-margin-bottom', 'row');
+        commentElement.style.display = 'flex';
 
         commentElement.innerHTML = `
           <div class="col-xs-3 col-sm-2">
@@ -44,58 +45,45 @@ const comments = () => {
           </div>
           <div class="col-xs-9 col-sm-9">
             <div class="review-inner review-arrow">
-              <p class="text-normal">${comment.author} ${itemIndex}</p>
+              <p class="text-normal">${comment.author}</p>
               <p>${comment.comment}</p>
             </div>
           </div>`;
 
         commentsContainer.append(commentElement);
-
         commentElements.push(commentElement);
 
         const currentElement = commentElement.querySelector('.review-inner');
         const imgItem = commentElement.querySelector('.col-xs-3.col-sm-2');
-        const bgColorIndex = itemIndex % bgColors.length;
-        const positionIndex = itemIndex % blockPosition.length;
 
+        if (comment.id % 2) {
+          imgItem.style.order = '1';
+        }
+
+        const bgColorIndex = comment.id % bgColors.length;
+        const positionIndex = comment.id % blockPosition.length;
+
+        currentElement.classList.add(bgColors[bgColorIndex]);
         currentElement.classList.add(bgColors[bgColorIndex], blockPosition[positionIndex]);
-
-        itemIndex++;
       });
 
       const showComments = () => {
         commentElements.forEach((elem) => {
           elem.style.display = 'none';
         });
-        for (let i = 0; i < 3; i++) {
-          const indexToShow = (itemIndex + i) % commentElements.length;
-          commentElements[indexToShow].style.display = 'flex';
-
-          const imageElement = commentElements[indexToShow].querySelector('.col-xs-3.col-sm-2');
-
-          const style = document.createElement('style');
-
-          if (i === 1) {
-            imageElement.style.order = '1';
-
-            style.textContent = `
-            .review-arrow-right::after {
-                right: -39px; 
-            }`;
-            document.head.appendChild(style);
-          } else {
-            imageElement.style.order = '0';
-            style.textContent = `
-            .review-arrow-right::after {
-                right: 39px; 
-            }`;
-          }
+        for (let i = itemIndex; i < itemIndex + 3; i++) {
+          commentElements[i % commentElements.length].style.display = 'flex';
         }
         itemIndex = (itemIndex + 1) % commentElements.length;
+
+        if (itemIndex === 4) {
+          itemIndex = 0;
+        }
       };
+
       showComments();
       spinner.style.display = 'none';
-      setInterval(showComments, 8000);
+      setInterval(showComments, 20000);
     });
   };
 
